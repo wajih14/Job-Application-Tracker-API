023d2b5ab01e869896507e596736487a0bf6b2f5
emna <emabou05@gmail.com>
2026-08-20 19:17:33 +0200

    Add PUT and PATCH application endpoints

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ class Application(BaseModel):
     position: str
     status: str
 
+class StatusUpdate(BaseModel):
+    status: str
+
 with open("test_data.json") as file:
     applications = json.load(file)
 
@@ -26,7 +29,7 @@ def get_applications():
 @app.post("/applications")
 def create_application(application: Application):
     global id
-    application = {"id": id, **application.dict()}
+    application = {"id": id, **application.model_dump()}
     id += 1
     applications.append(application)
     return application
@@ -45,4 +48,23 @@ def delete_application(application_id: int):
             applications.remove(application)
             return {"message": "Application deleted"}
     raise HTTPException(status_code=404, detail = "Application not found")
-    
+
+@app.put("/applications/{application_id}")
+def update_application_full(application_id: int, new_application_data: Application):
+    for application in applications:
+        if application["id"] == application_id:
+            application["company"] = new_application_data.company
+            application["position"] = new_application_data.position
+            application["status"] = new_application_data.status
+            return application
+            
+    raise HTTPException(status_code=404, detail = "Application not found")
+
+@app.patch("/applications/{application_id}")
+def update_application_status(application_id: int, new_status: StatusUpdate):
+    for application in applications:
+        if application["id"] == application_id:
+            application["status"] = new_status.status
+            return {"status": new_status}
+    raise HTTPException(status_code = 404, detail = "Application not found")
+
